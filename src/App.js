8720c98e8226057ef31f1a7e8dc36456afb495b0
David Martin <david.martin@fundingcircle.com>
2019-05-07 23:22:24 +0100

    Allow changing todo name

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ export function toggleTodoCompleted(state, id) {
   return R.over(R.lensPath(["todos", id, "completed"]), R.not, state);
 }
 
+export function addTodo(state) {
+  const newId = Math.max.apply(this, R.keys(state.todos)) + 1;
+  return R.assocPath(["todos", newId], {id: newId, name: "New Todo", completed: false}, state);
+}
+
+export function changeTodoName(state, id, newName) {
+  return R.assocPath(["todos", id, "name"], newName, state);
+}
+
 export class App extends React.Component {
   
   constructor(){
@@ -27,6 +36,16 @@ export class App extends React.Component {
       this.setState(newState);
     }
 
+    const updateStateAddTodo = () => {
+      const newState = addTodo(this.state);
+      this.setState(newState);
+    };
+
+    const updateStateChangeTodoName = (id, newName) => {
+      const newState = changeTodoName(this.state, id, newName);
+      this.setState(newState);
+    }
+
     return (
       <div>
         <header>
@@ -36,10 +55,11 @@ export class App extends React.Component {
           <p>Welcome to my awesome todo app 🙌</p>
           <ul>
           {R.values(this.state.todos).map(t => {
-            const todoDisplay = t.completed ? <s>{t.name}</s> : t.name;
+            const todoDisplay = t.completed ? <s>{t.name}</s> : <input onChange={(ev) => updateStateChangeTodoName(t.id, ev.target.value)} value={t.name}></input>; 
             return <li>{todoDisplay} <input type="checkbox" checked={t.completed} onChange={(_) => updateStateToggleTodoCompleted(t.id)}/></li>
           })}
           </ul>
+          <button onClick={(_) => updateStateAddTodo()}>Add a Todo</button>
         </content>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App, toggleTodoCompleted} from './App';
+import {App, toggleTodoCompleted, addTodo, changeTodoName} from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -37,3 +37,46 @@ describe('toggling a todo state', () => {
     expect(updatedState.todos[1].completed).toBe(false);
   });
 });
+
+describe('adding a new todo', () => {
+  it('should add a new todo', () => {
+    const state = {
+      todos: {
+        1: {
+          id: 1,
+          name: "Test Todo",
+          completed: false
+        }
+      }
+    };
+    const updatedState = addTodo(state);
+    expect(updatedState.todos).toEqual({
+      1: {
+        id: 1,
+        name: "Test Todo",
+        completed: false
+      },
+      2: {
+        id: 2,
+        name: "New Todo",
+        completed: false
+      }
+    })
+  })
+});
+
+describe("changing a todo's name", () => {
+  it('should update the name to the new name', () => {
+    const state = {
+      todos: {
+        1: {
+          id: 1,
+          name: "Old name",
+          completed: false
+        }
+      }
+    };
+    const updatedState = changeTodoName(state, 1, "New name");
+    expect(updatedState.todos[1].name).toEqual("New name");
+  });
+})
